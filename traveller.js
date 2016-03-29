57b891d231db87fdd1fcdfefe34c288d95abdab2
Divine <dubedivine@gmail.com>
2016-03-29 13:56:31 +0200

    done with ch07

--- traveller.js
+++ traveller.js
@@ -4,14 +4,20 @@ var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 var fortuneGen = require('./lib/fortune.js');
 
 var app = express();
-app.disable('X-Powered-By')
+app.disable('x-powered-by');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 app.use(function(err, req, res, next){
-	res.local.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
+app.use(function(req, res, next) {
+	if(!res.locals.partials) res.locals.partials = {} //partials context =  object. available to all views
+	res.locals.partials.weather = getWeatherData(); //creating a weather object/ weather context 
 	next();
 });
 
@@ -46,5 +52,35 @@ app.use(function(err, req, res, next){
 	res.status(500);
 	res.render('500');
 });
+
+
+function getWeatherData(){  //TODO: should put this in its own library
+return {
+locations: [
+{
+name: 'Portland',
+forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+weather: 'Overcast',
+temp: '54.1 F (12.3 C)',
+},
+{
+name: 'Bend',
+forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+weather: 'Partly Cloudy',
+temp: '55.0 F (12.8 C)',
+},
+{
+name: 'Manzanita',
+forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+weather: 'Light Rain',
+temp: '55.0 F (12.8 C)',
+},
+],
+};
+}
+
 
 app.listen(3000, function(){ console.log("APP STARTED ON PORT 3000---->>>"); });
